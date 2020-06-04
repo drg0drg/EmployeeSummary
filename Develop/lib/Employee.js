b768c26af178d30ee8d2cc9ee9f6abb1132e48db
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-06-04 18:17:52 +0100

    Employee master class and Manager, Engineer, Intern subclasses created. Tests ok

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,28 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+  constructor (name = "", id = "", email = "") {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+  }
+
+  getName() {
+      return this.name;
+  }
+  
+  getId() {
+      return this.id;
+  }
+  
+  getEmail() {
+      return this.email;
+  }
+
+  getRole() {
+      return "Employee";
+  }
+}
+
+// Export the Employee class so that it can be required in by other files
+module.exports = Employee;
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+  constructor (name, id, email, github){
+    super (name, id, email);
+    this.github = github;
+  }
+
+  getRole() {
+    return "Engineer";//Employee class has getRole() but this is overridden to output "Engineer"
+  }
+
+  getGithub() {
+    return this.github;
+  }
+}
+
+module.exports = Engineer;
+
