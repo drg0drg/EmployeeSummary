8430e7563c3763004b30887cff8ec28c20d4284c
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-06-04 22:15:53 +0100

    All prompt functions completed

--- Develop/app.js
+++ Develop/app.js
@@ -4,103 +4,160 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+employees = [];
 
+// function that creates team template
+async function generateTeam() {
+  try {
+    const userInput = await promptAllQuestions();
+  } catch (error) {
+    console.log(error);
+  }
+}
+generateTeam();
 
-// function to prompt questions on Manager role
-function promptManager() {
-  return inquirer.prompt([{
-      message : "Enter Manager's name:",
-      name : "name"
-  },
-  {
-      message : "Enter Manager's id:",
-      name : "id"
-  },
-  {
-      message : "Enter Manager's email:",
-      name : "email"
-  },
-  {
-      message : "Enter Manager's officeNumber:",
-      name : "officeNumber"
-  }])
-};
-promptManager();
+//function to prompt all questions
+async function promptAllQuestions() {
+  try {
+    await promptTeam();
+    await promptManager();
+    await pickEmployeeType();
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-// function to prompt questions on Engineer
-function promptEngineer(){
-  return inquirer.prompt([{
-      message : "Enter Engineer's name:",
-      name : "name"
-  },
-  {
-      message : "Enter Engineer's id:",
-      name : "id"
-  },
-  {
-      message : "Enter Engineer's email:",
-      name : "email"
-  },
-  {
-      message : "Enter Engineer's github:",
-      name : "github"
-  }])
-};
-// function to prompt questions on Intern
-function promptIntern(){
-  return inquirer.prompt([{
-      message : "Enter intern's name:",
-      name : "name"
-  },
-  {
-      message : "Enter intern's id:",
-      name : "id"
-  },
-  {
-      message : "Enter intern's email:",
-      name : "email"
-  },
-  {
-      message : "Enter intern's school:",
-      name : "school"
-  }])
-};
-
+//function to prompt questions on the specific team
+function promptTeam() {
+  return inquirer.prompt([
+    {
+      message: "Enter Team's name:",
+      name: "name",
+    },
+  ]);
+}
 
 // function to determine if the next team member is either an Engineer or an Intern
-function pickEmployeeType (){
-  return inquirer.prompt([{
-    type : "list",
-    message : "What type of employee is next ?", 
-    name : "typeOfEmployee",
-    choices : ["Engineer","Intern"]
-  }, 
-])
+async function pickEmployeeType() {
+  try {
+    let { employeeType } = await inquirer.prompt([
+      {
+        type: "list",
+        message: "What type of employee is next ?",
+        name: "employeeType",
+        choices: ["Engineer", "Intern"],
+      },
+    ]);
+
+    if (employeeType === "Engineer") {
+      promptEngineer();
+    } else if (employeeType === "Intern") {
+      promptIntern();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// function to prompt questions on Manager role
+async function promptManager() {
+  try {
+    const managerDetails = await inquirer.prompt([
+      {
+        message: "Enter Manager's name:",
+        name: "name",
+      },
+      {
+        message: "Enter Manager's id:",
+        name: "id",
+      },
+      {
+        message: "Enter Manager's email:",
+        name: "email",
+      },
+      {
+        message: "Enter Manager's officeNumber:",
+        name: "officeNumber",
+      },
+    ]);
+    const manager = new Manager(
+      managerDetails.name,
+      managerDetails.id,
+      managerDetails.email,
+      managerDetails.officeNumber
+    );
+    employees.push(manager);
+  } catch (error) {
+    console.log(error);
+  }
+}
+// promptManager();
+
+// function to prompt questions on Engineer
+async function promptEngineer() {
+  try {
+    const engineerDetails = await inquirer.prompt([
+      {
+        message: "Enter Engineer's name:",
+        name: "name",
+      },
+      {
+        message: "Enter Engineer's id:",
+        name: "id",
+      },
+      {
+        message: "Enter Engineer's email:",
+        name: "email",
+      },
+      {
+        message: "Enter Engineer's github:",
+        name: "github",
+      },
+    ]);
+    const engineer = new Engineer(
+      engineerDetails.name,
+      engineerDetails.id,
+      engineerDetails.email,
+      engineerDetails.github
+    );
+    employees.push(engineer);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// function to prompt questions on Intern
+async function promptIntern() {
+  try {
+    const internDetails = await inquirer.prompt([
+      {
+        message: "Enter Intern's name:",
+        name: "name",
+      },
+      {
+        message: "Enter Intern's id:",
+        name: "id",
+      },
+      {
+        message: "Enter Intern's email:",
+        name: "email",
+      },
+      {
+        message: "Enter Intern's school:",
+        name: "school",
+      },
+    ]);
+    const intern = new Engineer(
+      internDetails.name,
+      internDetails.id,
+      internDetails.email,
+      internDetails.school
+    );
+    employees.push(intern);
+  } catch (error) {
+    console.log(error);
+  }
 }
